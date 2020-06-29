fa53d62f266a0bd73a6b57fedbe03e0e93831ea6
Jon Haron-Feiertag <jharonfe@gmail.com>
2020-06-28 20:51:00 -0700

    Add aggregator project under yang project folder.

---
+++ yang/precache-manifest.b84346ed790b1d0f2989766ad6ae7c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413221e1cac6eff40ec404c91943fc9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a529e660ef59afd924b9",
+    "url": "/static/css/main.fc00915f.chunk.css"
+  },
+  {
+    "revision": "6a0ae4d1d8544e5d2f40",
+    "url": "/static/js/2.436c152a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.436c152a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a529e660ef59afd924b9",
+    "url": "/static/js/main.2b15ebd7.chunk.js"
+  },
+  {
+    "revision": "8c7b8fc1255c7a7edd50",
+    "url": "/static/js/runtime-main.beb346b5.js"
+  }
+]);
