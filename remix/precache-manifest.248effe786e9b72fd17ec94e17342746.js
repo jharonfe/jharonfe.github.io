84a9c40dcbb39493c7a96e47498e5f88da8d7f41
Jon Haron-Feiertag <jharonfe@gmail.com>
2020-06-30 16:52:44 -0700

    Add draft remix link.

---
+++ remix/precache-manifest.248effe786e9b72fd17ec94e17342746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5582e06e67e79d1b173485ac50cbfa18",
+    "url": "/remix/index.html"
+  },
+  {
+    "revision": "f0200667798ee2ee6e0f",
+    "url": "/remix/static/css/main.03e0b66b.chunk.css"
+  },
+  {
+    "revision": "e3579a9efc3b54e97395",
+    "url": "/remix/static/js/2.933f22c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/remix/static/js/2.933f22c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0200667798ee2ee6e0f",
+    "url": "/remix/static/js/main.19e5b78b.chunk.js"
+  },
+  {
+    "revision": "9298e356d93781243146",
+    "url": "/remix/static/js/runtime-main.be4cfac7.js"
+  }
+]);
