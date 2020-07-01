cbf76e375fca4f4b098788b43ad4fd18fc5c75f6
Jon Haron-Feiertag <jharonfe@gmail.com>
2020-07-01 15:33:35 -0700

    Update date filter threshold.

---
+++ yang/precache-manifest.d407f4312e5644b83c2eeb468274059a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac739cab61232959211538f4799523a",
+    "url": "/yang/index.html"
+  },
+  {
+    "revision": "3117914d8ea0030ccbb3",
+    "url": "/yang/static/css/main.b2e7ba27.chunk.css"
+  },
+  {
+    "revision": "64532ab399af4b5fcf46",
+    "url": "/yang/static/js/2.f027b018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yang/static/js/2.f027b018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3117914d8ea0030ccbb3",
+    "url": "/yang/static/js/main.f4fe391f.chunk.js"
+  },
+  {
+    "revision": "725649ef0c79b615e389",
+    "url": "/yang/static/js/runtime-main.974e203a.js"
+  }
+]);
